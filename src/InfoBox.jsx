98dc0d94ef57c38e4bce87f49c244af48eb0d8fb
erharshmishra <harshmishra8103@gmail.com>
2026-01-02 23:20:50 +0530

    added initial developed project

---
+++ src/InfoBox.jsx
@@ -0,0 +1,66 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "./InfoBox.css";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import SunnyIcon from "@mui/icons-material/Sunny";
+
+export default function InfoBox({ info }) {
+  let INIT_URL =
+    "https://images.unsplash.com/photo-1601134467661-3d775b999c8b?auto=format&fit=crop&w=800&q=80";
+
+  const HOT_URL =
+    "https://images.unsplash.com/photo-1561647784-2f9c43b07a0b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const COLD_URL =
+    "https://images.unsplash.com/photo-1612208695882-02f2322b7fee?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const RAIN_URL =
+    "https://images.unsplash.com/photo-1603262439120-3e17d13bab3f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  return (
+    <div className="InfoBox">
+      <div className="cardContainer">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL
+            }
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {info.city}&nbsp;
+              {
+              info.humidity > 80
+                ? <ThunderstormIcon/>
+                : info.temp > 15
+                ? <SunnyIcon/>
+                : <AcUnitIcon/>
+            }
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "text.secondary" }}
+              component={"span"}
+            >
+              <p>Temperature : {info.temp}&deg;C</p>
+              <p>Humidity : {info.humidity}</p>
+              <p>Min Temp : {info.tempMin}&deg;C</p>
+              <p>Max Temp : {info.tempMax}&deg;C</p>
+              <p>
+                The weather can be described as <b>{info.weather}</b> and feels
+                like <b>{info.feelsLike}&deg;C</b>
+              </p>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
